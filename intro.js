7aec6be0f9e6c4edac6223cb63c56e0f8f4ef481
Spencer Kendall <spencerlkendall@gmail.com>
2023-02-28 16:41:32 -0600

    added demo file

---
+++ intro.js
@@ -0,0 +1,32 @@
+//console.log("hello, world");
+
+const firstName = "Spencer";
+
+// console.log(firstName);
+
+let age = 27;
+
+const isCool = true;
+
+//console.log(firstName, age, isCool);
+
+// console.log("Hello, my name is " + firstName + "I am " + age + "years old.");
+
+// console.log("Hello, my name is ${firstName} I am ${age} years old.");
+
+let x = 4;
+let y = 10;
+let z = 7 + 8;
+let sum = x + y + z;
+
+// console.log(x);
+// console.log(y);
+// console.log(z);
+
+// console.log(sum);
+
+if (x === y){
+    console.log("X and Y are the same value")
+} else {
+    console.log("X and Y are different values")
+};
